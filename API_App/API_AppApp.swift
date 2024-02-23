6d0790f11035fe99189557d5fcba718c6809ff8d
Andres Cabral Lab <andres.cabralrs@udlap.mx>
2024-02-23 11:41:21 -0600

    Initial Commit

---
+++ API_App/API_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_AppApp.swift
+//  API_App
+//
+//  Created by iOS Lab on 23/02/24.
+//
+
+import SwiftUI
+
+@main
+struct API_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
